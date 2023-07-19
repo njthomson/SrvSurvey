9a88ec449f6bd4f1cb17e7dbb6cb404998253635
Nick Thomson <njthomson@outlook.com>
2023-07-18 22:12:38 -0700

    * doc updates

--- SrvSurvey/FormGroundTarget.Designer.cs
+++ SrvSurvey/FormGroundTarget.Designer.cs
@@ -30,157 +30,153 @@ namespace SrvSurvey
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGroundTarget));
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtLat = new System.Windows.Forms.TextBox();
-            this.txtLong = new System.Windows.Forms.TextBox();
-            this.btnBegin = new System.Windows.Forms.Button();
-            this.btnCancel = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnTargetCurrent = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label2 = new Label();
+            label3 = new Label();
+            txtLat = new TextBox();
+            txtLong = new TextBox();
+            btnBegin = new Button();
+            btnCancel = new Button();
+            label6 = new Label();
+            button1 = new Button();
+            btnTargetCurrent = new Button();
+            SuspendLayout();
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 81);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 15);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Latitute:";
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 81);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Latitute:";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 111);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 15);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "Longitude:";
+            label3.AutoSize = true;
+            label3.Location = new Point(14, 111);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Longitude:";
             // 
             // txtLat
             // 
-            this.txtLat.Location = new System.Drawing.Point(88, 77);
-            this.txtLat.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.txtLat.Name = "txtLat";
-            this.txtLat.Size = new System.Drawing.Size(186, 23);
-            this.txtLat.TabIndex = 0;
-            this.txtLat.Text = "+12.34";
+            txtLat.Location = new Point(88, 77);
+            txtLat.Margin = new Padding(4, 3, 4, 3);
+            txtLat.Name = "txtLat";
+            txtLat.Size = new Size(186, 23);
+            txtLat.TabIndex = 0;
+            txtLat.Text = "+12.34";
             // 
             // txtLong
             // 
-            this.txtLong.Location = new System.Drawing.Point(88, 107);
-            this.txtLong.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.txtLong.Name = "txtLong";
-            this.txtLong.Size = new System.Drawing.Size(186, 23);
-            this.txtLong.TabIndex = 2;
-            this.txtLong.Text = "-10.0";
+            txtLong.Location = new Point(88, 107);
+            txtLong.Margin = new Padding(4, 3, 4, 3);
+            txtLong.Name = "txtLong";
+            txtLong.Size = new Size(186, 23);
+            txtLong.TabIndex = 2;
+            txtLong.Text = "-10.0";
             // 
             // btnBegin
             // 
-            this.btnBegin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBegin.Location = new System.Drawing.Point(312, 165);
-            this.btnBegin.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btnBegin.Name = "btnBegin";
-            this.btnBegin.Size = new System.Drawing.Size(88, 27);
-            this.btnBegin.TabIndex = 4;
-            this.btnBegin.Text = "&Set target";
-            this.btnBegin.UseVisualStyleBackColor = true;
-            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
+            btnBegin.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnBegin.Location = new Point(312, 165);
+            btnBegin.Margin = new Padding(4, 3, 4, 3);
+            btnBegin.Name = "btnBegin";
+            btnBegin.Size = new Size(88, 27);
+            btnBegin.TabIndex = 4;
+            btnBegin.Text = "&Set target";
+            btnBegin.UseVisualStyleBackColor = true;
+            btnBegin.Click += btnBegin_Click;
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(406, 165);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(88, 27);
-            this.btnCancel.TabIndex = 5;
-            this.btnCancel.Text = "Cancel";
-            this.btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCancel.DialogResult = DialogResult.Cancel;
+            btnCancel.Location = new Point(406, 165);
+            btnCancel.Margin = new Padding(4, 3, 4, 3);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(88, 27);
+            btnCancel.TabIndex = 5;
+            btnCancel.Text = "Cancel";
+            btnCancel.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
-            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(14, 10);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(464, 60);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Got a tip from a mysterious stranger to go to some Lat/Long on some planet or moo" +
-    "n?\r\n\r\nEnter some Lat/Long position and guidance will appear when you approach.\r\n" +
-    "\r\n";
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label6.AutoSize = true;
+            label6.Location = new Point(14, 10);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(464, 60);
+            label6.TabIndex = 6;
+            label6.Text = "Got a tip from a mysterious stranger to go to some Lat/Long on some planet or moon?\r\n\r\nEnter some Lat/Long position and guidance will appear when you approach.\r\n\r\n";
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(18, 165);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 27);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "&Clear target";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button1.Location = new Point(18, 165);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 27);
+            button1.TabIndex = 3;
+            button1.Text = "&Clear target";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // btnTargetCurrent
             // 
-            this.btnTargetCurrent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnTargetCurrent.Location = new System.Drawing.Point(114, 165);
-            this.btnTargetCurrent.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.btnTargetCurrent.Name = "btnTargetCurrent";
-            this.btnTargetCurrent.Size = new System.Drawing.Size(160, 27);
-            this.btnTargetCurrent.TabIndex = 8;
-            this.btnTargetCurrent.Text = "&Target current location";
-            this.btnTargetCurrent.UseVisualStyleBackColor = true;
-            this.btnTargetCurrent.Click += new System.EventHandler(this.btnTargetCurrent_Click);
+            btnTargetCurrent.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnTargetCurrent.Location = new Point(114, 165);
+            btnTargetCurrent.Margin = new Padding(4, 3, 4, 3);
+            btnTargetCurrent.Name = "btnTargetCurrent";
+            btnTargetCurrent.Size = new Size(160, 27);
+            btnTargetCurrent.TabIndex = 8;
+            btnTargetCurrent.Text = "&Target current location";
+            btnTargetCurrent.UseVisualStyleBackColor = true;
+            btnTargetCurrent.Click += btnTargetCurrent_Click;
             // 
             // FormGroundTarget
             // 
-            this.AcceptButton = this.btnBegin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(507, 205);
-            this.Controls.Add(this.btnTargetCurrent);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnBegin);
-            this.Controls.Add(this.txtLong);
-            this.Controls.Add(this.txtLat);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "FormGroundTarget";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Select ground target";
-            this.Load += new System.EventHandler(this.FormGroundTarget_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AcceptButton = btnBegin;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnCancel;
+            ClientSize = new Size(507, 205);
+            Controls.Add(btnTargetCurrent);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(btnCancel);
+            Controls.Add(btnBegin);
+            Controls.Add(txtLong);
+            Controls.Add(txtLat);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FormGroundTarget";
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "Set lat/long co-ordinates";
+            Load += FormGroundTarget_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtLat;
-        private System.Windows.Forms.TextBox txtLong;
-        private System.Windows.Forms.Button btnBegin;
-        private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtLat;
+        private TextBox txtLong;
+        private Button btnBegin;
+        private Button btnCancel;
+        private Label label6;
+        private Button button1;
         private Button btnTargetCurrent;
     }
 }
