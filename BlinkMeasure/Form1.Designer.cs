72a423c0e444e2b003ed9e67ae3ffef118244f2a
Nick Thomson <njthomson@outlook.com>
2023-09-12 21:35:18 -0700

    - Rough blink measuring from screen grabs

---
+++ BlinkMeasure/Form1.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace BlinkMeasure
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            numHeight = new NumericUpDown();
+            label2 = new Label();
+            numWidth = new NumericUpDown();
+            numTop = new NumericUpDown();
+            label1 = new Label();
+            numLeft = new NumericUpDown();
+            groupBox2 = new GroupBox();
+            txtDiff = new TextBox();
+            label5 = new Label();
+            frameTwo = new Panel();
+            txtB = new TextBox();
+            txtG = new TextBox();
+            txtR = new TextBox();
+            label3 = new Label();
+            frameOne = new Panel();
+            numDelta = new NumericUpDown();
+            button2 = new Button();
+            groupBox3 = new GroupBox();
+            button3 = new Button();
+            label4 = new Label();
+            groupBox4 = new GroupBox();
+            button5 = new Button();
+            txtFolder = new TextBox();
+            button4 = new Button();
+            radFirst = new RadioButton();
+            radLast = new RadioButton();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numHeight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numWidth).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numTop).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numLeft).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numDelta).BeginInit();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(numHeight);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(numWidth);
+            groupBox1.Controls.Add(numTop);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(numLeft);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(200, 104);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Capture area:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(96, 75);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Set";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numHeight
+            // 
+            numHeight.Location = new Point(146, 46);
+            numHeight.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numHeight.Minimum = new decimal(new int[] { 10000, 0, 0, int.MinValue });
+            numHeight.Name = "numHeight";
+            numHeight.Size = new Size(44, 23);
+            numHeight.TabIndex = 5;
+            numHeight.Value = new decimal(new int[] { 60, 0, 0, 0 });
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Width, Height:";
+            // 
+            // numWidth
+            // 
+            numWidth.Location = new Point(96, 46);
+            numWidth.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numWidth.Minimum = new decimal(new int[] { 10000, 0, 0, int.MinValue });
+            numWidth.Name = "numWidth";
+            numWidth.Size = new Size(44, 23);
+            numWidth.TabIndex = 3;
+            numWidth.Value = new decimal(new int[] { 60, 0, 0, 0 });
+            // 
+            // numTop
+            // 
+            numTop.Location = new Point(146, 17);
+            numTop.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numTop.Minimum = new decimal(new int[] { 10000, 0, 0, int.MinValue });
+            numTop.Name = "numTop";
+            numTop.Size = new Size(44, 23);
+            numTop.TabIndex = 2;
+            numTop.Value = new decimal(new int[] { 260, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Top, Left:";
+            // 
+            // numLeft
+            // 
+            numLeft.Location = new Point(96, 17);
+            numLeft.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numLeft.Minimum = new decimal(new int[] { 10000, 0, 0, int.MinValue });
+            numLeft.Name = "numLeft";
+            numLeft.Size = new Size(44, 23);
+            numLeft.TabIndex = 0;
+            numLeft.Value = new decimal(new int[] { 260, 0, 0, 0 });
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(txtDiff);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(frameTwo);
+            groupBox2.Controls.Add(txtB);
+            groupBox2.Controls.Add(txtG);
+            groupBox2.Controls.Add(txtR);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(frameOne);
+            groupBox2.Location = new Point(12, 122);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(730, 235);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Captured image:";
+            // 
+            // txtDiff
+            // 
+            txtDiff.BackColor = Color.LightYellow;
+            txtDiff.Location = new Point(6, 144);
+            txtDiff.Name = "txtDiff";
+            txtDiff.ReadOnly = true;
+            txtDiff.Size = new Size(71, 23);
+            txtDiff.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(9, 126);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 15);
+            label5.TabIndex = 5;
+            label5.Text = "Difference:";
+            // 
+            // frameTwo
+            // 
+            frameTwo.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            frameTwo.BackColor = Color.Black;
+            frameTwo.BackgroundImageLayout = ImageLayout.Zoom;
+            frameTwo.BorderStyle = BorderStyle.Fixed3D;
+            frameTwo.Location = new Point(396, 16);
+            frameTwo.Name = "frameTwo";
+            frameTwo.Size = new Size(294, 213);
+            frameTwo.TabIndex = 1;
+            // 
+            // txtB
+            // 
+            txtB.Location = new Point(6, 95);
+            txtB.Name = "txtB";
+            txtB.ReadOnly = true;
+            txtB.Size = new Size(71, 23);
+            txtB.TabIndex = 4;
+            // 
+            // txtG
+            // 
+            txtG.Location = new Point(6, 66);
+            txtG.Name = "txtG";
+            txtG.ReadOnly = true;
+            txtG.Size = new Size(71, 23);
+            txtG.TabIndex = 3;
+            // 
+            // txtR
+            // 
+            txtR.Location = new Point(6, 37);
+            txtR.Name = "txtR";
+            txtR.ReadOnly = true;
+            txtR.Size = new Size(71, 23);
+            txtR.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 19);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Avg R, G, B:";
+            // 
+            // frameOne
+            // 
+            frameOne.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            frameOne.BackColor = Color.Black;
+            frameOne.BackgroundImageLayout = ImageLayout.Zoom;
+            frameOne.BorderStyle = BorderStyle.FixedSingle;
+            frameOne.Location = new Point(96, 16);
+            frameOne.Name = "frameOne";
+            frameOne.Size = new Size(294, 213);
+            frameOne.TabIndex = 0;
+            // 
+            // numDelta
+            // 
+            numDelta.Location = new Point(84, 17);
+            numDelta.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numDelta.Minimum = new decimal(new int[] { 10000, 0, 0, int.MinValue });
+            numDelta.Name = "numDelta";
+            numDelta.Size = new Size(44, 23);
+            numDelta.TabIndex = 7;
+            numDelta.Value = new decimal(new int[] { 60, 0, 0, 0 });
+            // 
+            // button2
+            // 
+            button2.Location = new Point(6, 46);
+            button2.Name = "button2";
+            button2.Size = new Size(122, 23);
+            button2.TabIndex = 8;
+            button2.Text = "Single compare";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Controls.Add(button2);
+            groupBox3.Controls.Add(numDelta);
+            groupBox3.Location = new Point(218, 16);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(139, 100);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Difference:";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(6, 71);
+            button3.Name = "button3";
+            button3.Size = new Size(122, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Trigger compare";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Threshold:";
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(radLast);
+            groupBox4.Controls.Add(radFirst);
+            groupBox4.Controls.Add(button5);
+            groupBox4.Controls.Add(txtFolder);
+            groupBox4.Controls.Add(button4);
+            groupBox4.Location = new Point(363, 16);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(379, 100);
+            groupBox4.TabIndex = 9;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Measure:";
+            // 
+            // button5
+            // 
+            button5.Enabled = false;
+            button5.Location = new Point(87, 71);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 2;
+            button5.Text = "Stop";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // txtFolder
+            // 
+            txtFolder.Location = new Point(6, 22);
+            txtFolder.Name = "txtFolder";
+            txtFolder.Size = new Size(367, 23);
+            txtFolder.TabIndex = 1;
+            txtFolder.Tag = "folder";
+            txtFolder.Text = "c:\\blink-measure";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(6, 71);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 0;
+            button4.Text = "Start";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // radFirst
+            // 
+            radFirst.AutoSize = true;
+            radFirst.Location = new Point(6, 48);
+            radFirst.Name = "radFirst";
+            radFirst.Size = new Size(138, 19);
+            radFirst.TabIndex = 11;
+            radFirst.TabStop = true;
+            radFirst.Text = "Delta from first frame";
+            radFirst.UseVisualStyleBackColor = true;
+            // 
+            // radLast
+            // 
+            radLast.AutoSize = true;
+            radLast.Location = new Point(150, 48);
+            radLast.Name = "radLast";
+            radLast.Size = new Size(136, 19);
+            radLast.TabIndex = 12;
+            radLast.TabStop = true;
+            radLast.Text = "Delta from last frame";
+            radLast.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(755, 377);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Blink Measure";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numHeight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numWidth).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numTop).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numLeft).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numDelta).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private NumericUpDown numHeight;
+        private Label label2;
+        private NumericUpDown numWidth;
+        private NumericUpDown numTop;
+        private Label label1;
+        private NumericUpDown numLeft;
+        private GroupBox groupBox2;
+        private Button button1;
+        private Panel frameOne;
+        private TextBox txtB;
+        private TextBox txtG;
+        private TextBox txtR;
+        private Label label3;
+        private NumericUpDown numDelta;
+        private Button button2;
+        private Panel frameTwo;
+        private GroupBox groupBox3;
+        private Label label4;
+        private TextBox txtDiff;
+        private Label label5;
+        private Button button3;
+        private GroupBox groupBox4;
+        private Button button4;
+        private TextBox txtFolder;
+        private Button button5;
+        private RadioButton radLast;
+        private RadioButton radFirst;
+    }
+}
---
+++ BlinkMeasure/Form1.cs
@@ -0,0 +1,359 @@
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Xml;
+using static System.Windows.Forms.LinkLabel;
+
+namespace BlinkMeasure
+{
+    public partial class Form1 : Form
+    {
+        private static int floaterBorderWidth = 2;
+        private static Pen floaterBorderPen = new Pen(Color.Red, floaterBorderWidth)
+        {
+            DashStyle = System.Drawing.Drawing2D.DashStyle.Dash,
+        };
+
+        private Form floater;
+        private Point pt;
+        private Size sz;
+        private RGB lastAvg;
+        private double threshold;
+        private int count;
+        private Bitmap lastImg;
+        private DateTime startTime;
+        private List<LineItem> lines = new List<LineItem>();
+        private bool measuring = false;
+        private bool stop = true;
+        private string folder;
+        private bool deltaFirst;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numLeft.Value = decimal.Parse((string)Application.UserAppDataRegistry.GetValue("left", "100"));
+            numTop.Value = decimal.Parse((string)Application.UserAppDataRegistry.GetValue("top", "100"));
+            numWidth.Value = decimal.Parse((string)Application.UserAppDataRegistry.GetValue("width", "100"));
+            numHeight.Value = decimal.Parse((string)Application.UserAppDataRegistry.GetValue("height", "100"));
+            numDelta.Value = decimal.Parse((string)Application.UserAppDataRegistry.GetValue("trigger", "20"));
+            txtFolder.Text = (string)Application.UserAppDataRegistry.GetValue("folder", "c:\\blink-measure\\1");
+            radFirst.Checked = bool.Parse((string)Application.UserAppDataRegistry.GetValue("deltaFirst", "true"));
+            radLast.Checked = !radFirst.Checked;
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.UserAppDataRegistry.SetValue("left", numLeft.Value);
+            Application.UserAppDataRegistry.SetValue("top", numTop.Value);
+            Application.UserAppDataRegistry.SetValue("width", numWidth.Value);
+            Application.UserAppDataRegistry.SetValue("height", numHeight.Value);
+            Application.UserAppDataRegistry.SetValue("trigger", numDelta.Value);
+            Application.UserAppDataRegistry.SetValue("folder", txtFolder.Text);
+            Application.UserAppDataRegistry.SetValue("deltaFirst", radFirst.Checked.ToString());
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.createFloater();
+        }
+
+        private void createFloater()
+        {
+            this.pt = new Point((int)numLeft.Value, (int)numTop.Value);
+            this.sz = new Size((int)numWidth.Value, (int)numHeight.Value);
+
+            if (this.floater == null)
+            {
+                this.floater = new Form()
+                {
+                    BackColor = Color.Blue,
+                    ShowIcon = false,
+                    ShowInTaskbar = false,
+                    MaximizeBox = false,
+                    MinimizeBox = false,
+                    ControlBox = false,
+                    StartPosition = FormStartPosition.Manual,
+                    Text = "",
+                    FormBorderStyle = FormBorderStyle.None,
+                    HelpButton = false,
+                    TopMost = true,
+                    AllowTransparency = true,
+                    TransparencyKey = Color.Blue,
+                };
+
+                this.floater.Paint += Floater_Paint;
+            }
+
+            if (!this.floater.Visible)
+                this.floater.Show();
+
+            Application.DoEvents();
+
+            this.floater.Left = pt.X - floaterBorderWidth;
+            this.floater.Top = pt.Y - floaterBorderWidth;
+            this.floater.Width = sz.Width + (floaterBorderWidth * 2);
+            this.floater.Height = sz.Height + (floaterBorderWidth * 2);
+
+            this.floater.Invalidate();
+
+            Application.DoEvents();
+
+            var img = this.grabFrame();
+
+            var c = this.getAverageRGB(img);
+            txtR.Text = c.r.ToString();
+            txtG.Text = c.g.ToString();
+            txtB.Text = c.b.ToString();
+
+            frameOne.BackgroundImage = img;
+            frameTwo.BackgroundImage = null;
+
+        }
+
+        private void Floater_Paint(object? sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(floaterBorderPen, 1, 1, this.floater.Width - floaterBorderWidth, this.floater.Height - floaterBorderWidth);
+        }
+
+        private Bitmap grabFrame()
+        {
+            // copy pixels
+            var img = new Bitmap(sz.Width, sz.Height);
+            using (var gr = Graphics.FromImage(img))
+            {
+                gr.CopyFromScreen(pt.X, pt.Y, 0, 0, img.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            return img;
+        }
+
+        private RGB getAverageRGB(Bitmap img)
+        {
+            // get average RGB values
+            var c = new RGB();
+            for (var x = 0; x < sz.Width; x++)
+            {
+                for (var y = 0; y < sz.Height; y++)
+                {
+                    var p = img.GetPixel(x, y);
+                    c.r += p.R;
+                    c.g += p.G;
+                    c.b += p.B;
+                }
+            }
+
+            var nn = sz.Width * sz.Height;
+
+            c.r /= nn;
+            c.g /= nn;
+            c.b /= nn;
+
+            return c;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (this.floater == null) this.createFloater();
+
+            var img = this.grabFrame();
+
+            if (this.lastAvg == null)
+            {
+                frameOne.BackgroundImage = img;
+                this.lastAvg = this.getAverageRGB(img);
+                return;
+            }
+
+            // diff threshold 
+
+            frameOne.BackgroundImage = frameTwo.BackgroundImage;
+            frameTwo.BackgroundImage = img;
+            var avg = this.getAverageRGB(img);
+
+            var delta = new RGB()
+            {
+                r = Math.Abs(lastAvg.r - avg.r),
+                g = Math.Abs(lastAvg.g - avg.g),
+                b = Math.Abs(lastAvg.b - avg.b),
+            };
+
+            txtR.Text = delta.r.ToString();
+            txtG.Text = delta.g.ToString();
+            txtB.Text = delta.b.ToString();
+
+
+            txtDiff.Text = delta.sum.ToString();
+
+            this.lastAvg = avg;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.startStuff();
+            this.Text = "Trigger testing...";
+            this.measuring = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.button4.Enabled = false;
+            this.button5.Enabled = true;
+            this.measuring = true;
+            this.startStuff();
+            this.Text = "Measuring ...";
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.button5.Enabled = false;
+            this.button4.Enabled = true;
+            this.stop = true;
+        }
+
+
+        private void startStuff()
+        {
+            if (this.floater == null)
+                this.createFloater();
+
+            this.stop = false;
+            this.lines.Clear();
+            this.count = 0;
+            this.threshold = (double)numDelta.Value;
+            this.startTime = DateTime.Now;
+            this.folder = txtFolder.Text;
+            this.deltaFirst = radFirst.Checked;
+
+            var img = this.grabFrame();
+            this.lastAvg = this.getAverageRGB(img);
+            frameOne.BackgroundImage = img;
+            frameTwo.BackgroundImage = null;
+
+            Task.Run(new Action(this.doStuff));
+        }
+
+        private void doStuff()
+        {
+            ++this.count;
+            var img = this.grabFrame();
+            var avg = this.getAverageRGB(img);
+
+            var delta = new RGB()
+            {
+                r = Math.Abs(lastAvg.r - avg.r),
+                g = Math.Abs(lastAvg.g - avg.g),
+                b = Math.Abs(lastAvg.b - avg.b),
+            };
+
+            var line = new LineItem()
+            {
+                duration = DateTime.Now.Subtract(this.startTime),
+                delta = delta.sum,
+            };
+            this.lines.Add(line);
+
+            if (delta.sum > this.threshold || this.lines.Count == 1)
+            {
+                line.img = img;
+
+                if (!measuring)
+                {
+                    this.stop = true;
+                }
+            }
+
+            if (!deltaFirst)
+                lastAvg = avg;
+
+            if (stop)
+            {
+                if (this.measuring)
+                    this.endStuff();
+
+                this.Invoke(new Action(() =>
+                {
+                    this.Text = $"count: {this.count}, {DateTime.Now.Subtract(this.startTime)}";
+
+                    txtR.Text = delta.r.ToString();
+                    txtG.Text = delta.g.ToString();
+                    txtB.Text = delta.b.ToString();
+
+                    txtDiff.Text = delta.sum.ToString();
+                    frameTwo.BackgroundImage = img;
+                }));
+            }
+            else
+            {
+                this.BeginInvoke(new Action(() =>
+                {
+                    txtDiff.Text = delta.sum.ToString();
+                }));
+
+                this.doStuff();
+            }
+        }
+
+        private void endStuff()
+        {
+            Directory.CreateDirectory(this.folder);
+            var str = new StringBuilder();
+            str.AppendLine("duration (seconds),delta,image path");
+
+            foreach (var line in this.lines)
+            {
+                var duration = line.duration.TotalSeconds.ToString("N3"); // }." + (line.duration.Milliseconds / 1000.0).ToString("N3");
+                var imgPath = "";
+                if (line.img != null)
+                {
+                    imgPath = Path.Combine(this.folder, $"img-{duration}.png");
+                    line.img.Save(imgPath, ImageFormat.Png);
+                }
+
+                str.AppendLine($"{duration},{line.delta},{imgPath}");
+            }
+
+            var csvPath = Path.Combine(this.folder, $"data.csv");
+            File.WriteAllText(csvPath, str.ToString());
+
+            this.Invoke(new Action(() =>
+            {
+                this.Text = "Stopped";
+
+                var info = new ProcessStartInfo(this.folder);
+                info.UseShellExecute = true;
+                Process.Start(info);
+            }));
+        }
+    }
+
+    class LineItem
+    {
+        public TimeSpan duration;
+        public double delta;
+        public Bitmap img;
+
+        public override string ToString()
+        {
+            return $"{duration.TotalMilliseconds} / {delta} / {img != null}";
+        }
+    }
+
+    class RGB
+    {
+        public double r;
+        public double g;
+        public double b;
+
+        public double sum { get => this.r + this.g + this.b; }
+    }
+}
